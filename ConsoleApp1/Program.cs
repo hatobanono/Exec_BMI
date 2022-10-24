127de3fb6a36e0dba8433ab167d918e2007acfc3
hatobanono <chris00iq@gmail.com>
2022-10-24 19:20:32 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double height = 173;
+			int weight = 80;
+			double bmi = weight / Math.Pow((height / 100.00), 2);
+			Console.WriteLine($"{bmi:0.00}");
+			
+
+		}
+	}
+}
